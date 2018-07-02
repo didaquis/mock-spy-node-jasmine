89641915c8a6e0502137f61bb8bd3a1d25b98cc4
Dídac García <didaquis@gmail.com>
2018-07-02 11:03:29 +0200

    First examples

---
+++ README.md
@@ -0,0 +1,24 @@
+# mock-spy-node-jasmine
+Esto es un ejemplo sencillo de uso de mocks, spies, stubs, fakes...
+
+## Instalar y ejecutar los test
+Descarga el repositorio e instala las dependencias: `npm install`.  
+
+Para ejecutar los tests, usa el comando: `npm test`.
+
+------
+
+## Introducción teórica de conceptos
+
+### Mocks: 
+Un mock es un "doble de prueba" (test double) que sustituye por completo el método/función/objeto original.
+
+### Spy:
+Afectan a un objeto, clase o función, pero no alteran el original, sino que se limitan a observar cosas, como el número de veces que se pasa por un método o la cantidad de argumentos que recibe.
+
+### Stub:
+Se comportan como los spies, pero además reemplazan alguna funcionalidad original, como el resultado que se devuelve después de un proceso
+
+
+> Este tipo de artilugios -los spies, stubs, fakes, mocks y demás- reciben el nombre de dobles, porque al igual que los dobles de las películas reemplazan al actor principal.
+
---
+++ spec/spy-spec.js
@@ -0,0 +1,58 @@
+// Code to test:
+let myObj = {
+	save: function() {
+		// ...
+	},
+	getQuantity: function() {
+		return 5;
+	},
+	printText: function(textToPrint) {
+		// ...
+	}
+}
+
+// ====================================================
+
+
+describe("Spies", () => {
+	//console.log(myObj);
+
+	it('Should spy on "save" method', () => {
+		let spy = spyOn(myObj, 'save');
+
+		expect(myObj.save).toBeDefined(); // comprobamos que el método existe.
+
+		myObj.save(); // llamamos al espía.
+		expect(spy).toHaveBeenCalled(); // comprobamos que el método "save" ha sido llamado.
+		expect(spy).toHaveBeenCalledTimes(1); // comprobamos el número de veces que llamamos al método "save".
+	});
+
+	it('Should spy on "getQuantity" method', () => {
+		let spy = spyOn(myObj, 'getQuantity').and.returnValue(10); // establecemos que el método "getQuantity" devolverá 10.
+		expect(myObj.getQuantity()).toEqual(10); // comprobamos que devuelve 10
+	});
+
+	it('Should spy on "getQuantity" fake', () => {
+		let spy = spyOn(myObj, 'getQuantity').and.callFake( () => {
+			//console.log('returning 20');
+			return 20;
+		});
+		expect(myObj.getQuantity()).toEqual(20); // comprobamos que devuelve 20
+	});
+
+	it('Should spy on "getQuantity" call through', () => {
+		let spy = spyOn(myObj, 'getQuantity').and.callThrough(); // esto nos permite llamar al método "real" en vez de al doble
+		
+		expect(myObj.getQuantity()).toEqual(5); // comprobamos que el método real devuelve 5
+		expect(spy).toHaveBeenCalled(); // comprobamos que el método "getQuantity" del objeto real ha sido llamado.
+	});
+
+	it('Should be possible called "printText" method with 1 parameter', () => {
+		let spy = spyOn(myObj, 'printText');
+
+		myObj.printText('foo');
+		expect(myObj.printText).toHaveBeenCalledWith('foo'); // comprobamos que podemos enviarle un argumento.
+		// En realidad, este último test nos da un falso positivo, ya que en javaScript los métodos no fallan si se les envía parámetros de más o de menos.
+	});
+
+});
