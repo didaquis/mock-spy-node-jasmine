cafa17af9e5e53c340342d3be40b5be0b13e4206
Dídac García <didaquis@gmail.com>
2020-03-01 16:17:31 +0100

    Added link for reference

--- spec/myModule.spec.js
+++ spec/myModule.spec.js
@@ -20,7 +20,7 @@ describe('myModule', () => {
 	});
 
 	// El siguiente test falla porque no consigo rehacer el comportamiento del espía. Sin embargo, el test pasa correctamente si el primer test está desactivado. De algún modo debería restaurar el comportamiento de 'foo' y 'bar', pero no logro hacerlo
-	// https://github.com/jasmine/jasmine/issues/1798
+	// https://github.com/jasmine/jasmine/issues/1798 y https://stackoverflow.com/questions/60149391/how-to-use-jasmine-js-spy-on-a-required-function
 
 	// describe('bizz should return fake-other-data', () => {
 	// 	it('when foo returns false', () => {
